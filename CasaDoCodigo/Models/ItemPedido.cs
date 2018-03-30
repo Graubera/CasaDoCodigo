52809268b664d0ef96e2aa63ad9971d5957f8ac1
Glauber Soares Bianchi Gualda <glaubergualda@hotmail.com>
2018-03-30 19:25:40 -0300

    implementing models

---
+++ CasaDoCodigo/Models/ItemPedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class ItemPedido
+    {
+        public ItemPedido(int id, Produto produto, int quantidade)
+        {
+            this.Id = id;
+            this.Produto = produto;
+            this.Quantidade = quantidade;
+            this.PrecoUnitario = produto.Preco;
+        }
+        public int Id { get; private set; }
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; private set; }
+        public decimal Subtotal { get { return Quantidade * PrecoUnitario; } }
+
+
+    }
+}
